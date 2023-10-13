8a75165448127a168f203a506e7a055092ef03dd
Anthony Chavis <gitanthony@yahoo.com>
2023-10-13 00:32:11 -0400

    typed fxn

---
+++ app/components/card.tsx
@@ -0,0 +1,9 @@
+import style from './card.module.css';
+
+type Props = {
+    children: JSX.Element[] | JSX.Element;
+};
+
+export const Card = ({ children }: Props) => (
+    <article className={style.card}>{children}</article>
+);
