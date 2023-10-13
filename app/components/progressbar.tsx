25c7bf65c03d1881f56a094380e053a5ada2200c
Anthony Chavis <gitanthony@yahoo.com>
2023-10-13 02:30:49 -0400

    genesis

---
+++ app/components/progressbar.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    children: string;
+};
+
+// temp fxn component body
+export const ProgressBar = ({ children }: Props) => (
+    // <figure>
+    //     <figcaption>{children}</figcaption>
+    // </figure>
+    <p>{children}</p>
+);
