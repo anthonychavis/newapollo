b48d783ca547bbd29b63565494d1f5ff8c47023b
Anthony Chavis <gitanthony@yahoo.com>
2023-10-13 02:29:37 -0400

    class in attributes & props

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -2,8 +2,11 @@ import style from './card.module.css';
 
 type Props = {
     children: JSX.Element[] | JSX.Element;
+    className?: string;
 };
 
 export const Card = ({ children }: Props) => (
-    <article className={style.card}>{children}</article>
+    <>
+        <article className={style.card}>{children}</article>
+    </>
 );
