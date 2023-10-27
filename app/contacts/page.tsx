fb823db78fde7a58162a501611f9895da21a9409
Anthony Chavis <gitanthony@yahoo.com>
2023-10-27 15:13:28 -0400

    git init

---
+++ app/contacts/page.tsx
@@ -0,0 +1,9 @@
+import Main from '../_components/MainComp/main';
+
+export default function Contacts() {
+    return (
+        <Main>
+            <h2>Contacts</h2>
+        </Main>
+    );
+}
---
+++ app/features/page.tsx
@@ -0,0 +1,9 @@
+import Main from '../_components/MainComp/main';
+
+export default function Features() {
+    return (
+        <Main>
+            <h2>Features</h2>
+        </Main>
+    );
+}
